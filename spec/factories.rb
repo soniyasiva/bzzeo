0e15496f66265fd61df3ace9719726d84f036d02
Zachary Levy <zacharyalevy@gmail.com>
2015-10-24 12:07:30 -0400

    devise spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
 
+  factory :user do
+    email "zacharyalevy@gmail.com"
+    password "password"
+    password_confirmation "password"
+    # confirmed_at Date.today
+  end
+
   factory :category do
     name "Real Estate"
   end
