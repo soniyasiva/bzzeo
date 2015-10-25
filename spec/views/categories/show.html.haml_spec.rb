2366fc983680ba7e1cb508440580118f436c408f
Zachary Levy <zacharyalevy@gmail.com>
2015-10-24 21:08:42 -0400

    spec with factories

--- spec/views/categories/show.html.haml_spec.rb
+++ spec/views/categories/show.html.haml_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "categories/show", type: :view do
   before(:each) do
-    @category = assign(:category, Category.create!(
-      :name => "Name"
-    ))
+    @category = assign(:category, create(:category))
   end
 
   it "renders attributes in <p>" do
--- spec/views/tags/show.html.haml_spec.rb
+++ spec/views/tags/show.html.haml_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "tags/show", type: :view do
   before(:each) do
-    @tag = assign(:tag, Tag.create!(
-      :name => "Name"
-    ))
+    @tag = assign(:tag, create(:tag))
   end
 
   it "renders attributes in <p>" do
