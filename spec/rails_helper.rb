9aa770235424bbc09db28107f7b5e0744f2d8669
Zachary Levy <zacharyalevy@gmail.com>
2015-10-26 01:12:25 -0400

    capybara config, login helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
 require 'support/controller_macros'
+require 'support/feature_macros'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,6 +34,8 @@ RSpec.configure do |config|
   # devise
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, :type => :controller
+  config.include Capybara::DSL
+  config.extend FeatureMacros, type: :feature
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
