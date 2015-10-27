8d5749491677ff3f2478222c90280f2f04f511dc
Zachary Levy <zacharyalevy@gmail.com>
2015-10-27 12:29:09 -0400

    expose @user in feature specs

--- spec/support/feature_macros.rb
+++ spec/support/feature_macros.rb
@@ -3,9 +3,9 @@ include Warden::Test::Helpers
 module FeatureMacros
   def login
     before :each do
-      user = create(:user)
-      login_as user, scope: :user
-      user
+      @user = create(:user)
+      login_as @user, scope: :user
+      @user
     end
   end
 end
