3715edf1fecc57b4e7722ea693bf639802841fdb
Zachary Levy <zacharyalevy@gmail.com>
2015-11-10 16:40:25 -0500

    friend validations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,7 @@
 class Friend < ActiveRecord::Base
   belongs_to :profile
   belongs_to :friend, class_name: "Profile"
+
+  validates :profile, presence: true
+  validates :friend, presence: true
 end
