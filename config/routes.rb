be136b0ef7ac7152cc29d1e9c5f291672b3a5936
Zachary Levy <zacharyalevy@gmail.com>
2015-11-12 13:12:46 -0500

    ui

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users
-  root to: "categories#index"
+  root to: "posts#index"
   resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
