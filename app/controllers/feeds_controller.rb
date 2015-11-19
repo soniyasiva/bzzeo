ba7cf653e9cf23e060e51d438d8f12ea6f11bcd7
Zachary Levy <zacharyalevy@gmail.com>
2015-11-18 23:32:48 -0500

    feed basic combined

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,7 +3,9 @@ class FeedsController < ApplicationController
   # GET /feeds
   # GET /feeds.json
   def index
-    @feeds = Post.all
+    posts = Post.all
+    shares = Share.all
+    @items = (posts.to_a + shares.to_a).sort_by(&:created_at)
   end
 
 end
