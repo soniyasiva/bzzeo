db5ce532d8066d6ae9c7d738b7f04baa4ee7bb19
Zachary Levy <zacharyalevy@gmail.com>
2015-11-19 15:16:03 -0500

    refactor search, search ui

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -10,19 +10,57 @@ class FeedsController < ApplicationController
 
   def search
     puts "===== search ====="
-    query = params[:query]
+    # sets vars or nil
+    @category = params[:category_id] unless params[:category_id].blank?
+    @query = params[:query] unless params[:query].blank?
+    @address = geocode_address params[:address] unless params[:address].blank?
+
+    # gets profiles
+    profiles = Profile.all
     # searches profiles for
     # profile name
     # profile tag names
-    profiles = Profile.joins(:tags).where("profiles.name ILIKE ? OR tags.name ILIKE ?", "%#{query}%", "%#{query}%").uniq
-    # searches posts for
-    # description
-    posts = Post.includes(:profile, {comments: :profile}, :likes).where("description ILIKE ?", "%#{query}%")
-    @items = (posts.to_a + profiles.to_a).sort_by(&:created_at)
+    profiles = profiles.joins(:tags).where("profiles.name ILIKE ? OR tags.name ILIKE ?", "%#{@query}%", "%#{@query}%").uniq unless @query.nil?
+    # filter by category
+    profiles = profiles.where(category_id: @category) unless @category.nil?
+    # sort by distance
+    # within 100km, sorted by distance from origin, closest first
+    profiles = profiles.within(100, :origin => @address).order(address: :asc) unless @address.nil?
+
+    # searches posts also
+    if @category.nil? && @address.nil?
+      # gets posts
+      posts = Post.includes(:profile, {comments: :profile}, :likes)
+      # searches posts for
+      # description
+      posts = posts.where("description ILIKE ?", "%#{@query}%") unless @query.nil?
+      # preorder posts by created_at
+      posts = posts.order(created_at: :desc)
+      # combine the two result times and sort by created_at desc
+      @items = (posts.to_a + profiles.to_a).sort_by do |item|
+        item.created_at
+      end.reverse!
+    else
+      @items = profiles
+    end
+
+
     # debug
     puts "==== items ====="
     puts @items
     render 'search_results'
+  end
+
+  private
+  def geocode_address address
+    return nil if address.blank?
+    geo = Geokit::Geocoders::MultiGeocoder.geocode address
+    if geo.success
+      geo
+    else
+      flash[:notice] = "Address not valid"
+      nil
+    end
   end
 
 end
