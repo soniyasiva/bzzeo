99ac1ef65b3661e7fcbecd885823e1690f28a4e6
Zachary Levy <zacharyalevy@gmail.com>
2015-11-28 01:30:23 -0500

    force profile name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # before_action :ensure_profile
-
+  before_action :ensure_profile
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
 
-  # def ensure_profile
-  #   if !current_user.nil? && current_user.profile.name.blank? && request.path != edit_profile_path(current_user.profile) && request.path != profile_path(current_user.profile)
-  #
-  #     redirect_to edit_profile_path(current_user.profile)
-  #     flash[:notice] = "Please fill our your profile."
-  #   end
-  # end
+  def ensure_profile
+    if user_signed_in? && current_user.profile.name.blank? && request.path != edit_profile_path(current_user.profile) && request.path != profile_path(current_user.profile)
+      redirect_to edit_profile_path(current_user.profile)
+      flash[:notice] = "Please fill our your profile."
+    end
+  end
 end
