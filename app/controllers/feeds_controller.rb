3e59d1a8a81e234ca91e6ec86bfbe18108337050
Zachary Levy <zacharyalevy@gmail.com>
2015-12-18 11:55:35 -0500

    feed tags for posts

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -38,9 +38,11 @@ class FeedsController < ApplicationController
   def tag
     @profiles = Profile.joins(:tags).where("tags.name = ?", params[:tag]) unless params[:tag].nil?
     @profiles = @profiles.paginate(:page => params[:page], :per_page => 10)
+    @posts = Post.joins(:tags).where("tags.name = ?", params[:tag]) unless params[:tag].nil?
+    @posts = @posts.paginate(:page => params[:page], :per_page => 10)
     puts params[:tag]
     puts @profiles
-    @items = @profiles
+    @items = (@profiles.to_a + @posts.to_a).sort_by(&:created_at).reverse!
     render 'index'
   end
 
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,4 +1,7 @@
 class PostTag < ActiveRecord::Base
   belongs_to :post
   belongs_to :tag
+
+  validates :profile, presence: true
+  validates :tag, presence: true
 end
